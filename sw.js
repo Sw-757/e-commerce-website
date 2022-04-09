0ea3586e22564d8686bd872e516ad5b9a9b98849
Sw-757 <88151052+Sw-757@users.noreply.github.com>
2022-04-09 19:33:40 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,72 @@
+self.addEventListener('install' , (event)=>{
+    console.log("sw is installed")
+    event.waitUntil(
+    caches.open("static")
+    .then((Cache)=>{
+        Cache.addAll([
+           'https://sw-757.github.io/e-commerce-website/',
+           'https://sw-757.github.io/e-commerce-website/css/style.css',
+           'https://sw-757.github.io/e-commerce-website/js/app.js',
+           'https://sw-757.github.io/e-commerce-website/index.html'
+ 
+        ]).catch((error)=>{
+            console.log(error)
+        })
+    })
+    );
+})
+
+self.addEventListener('activate' , ()=>{
+    console.log("sw is Activated")
+})
 
 
-self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('static').then(function(cache) {
-     return cache.addAll([
-        '/',
-        '/css/style.css',
-        '/js/app.js',
-        '/index.html'      
-     ]);
-   })
- );
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // The responce is in the cache
+        if (response) {
+          return response;
+        }
+
+        // No cache match, we attempt to fetch it from the network
+        return fetch(event.request);
+      }
+    )
+  );
 });
 
-self.addEventListener('fetch', function(event) {
-    console.log(event.request.url);
-   });
+
+self.addEventListener('push', e=> {
+console.log('push', e);
+var body;
+
+if (e.data) {
+body = e.data.text();
+} else {
+body = 'Push message no payload';
+}
+var options = {
+body: body,
+icon: 'watch.jpg',
+vibrate: [100, 50, 100],
+data: {
+dateOfArrival: Date.now(),
+primaryKey: 1
+},
+actions: [
+{action: 'explore', title: 'Explore this new world',
+icon: 'shirt.jpg'},
+{action: 'close', title: 'I don\'t want any of this',
+icon: 'watch.jpg'},
+]
+};
+e.waitUntil(
+self.registration.showNotification('Push Notification', options)
+);
+});
+
+self.addEventListener('sync', function(event) {
+	console.log("sync event", event);
+});
