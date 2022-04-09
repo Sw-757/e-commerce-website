6aee14d283b00c39e2b3163aa2f0c9352336c517
Sw-757 <88151052+Sw-757@users.noreply.github.com>
2022-04-09 17:59:46 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,13 @@
 
-importScripts('/cache-polyfill.js');
-
 
 self.addEventListener('install', function(e) {
  e.waitUntil(
-   caches.open('airhorner').then(function(cache) {
+   caches.open('static').then(function(cache) {
      return cache.addAll([
-       '/',
-       '/e-commerce-website',
-       '/index.html',
-       '/index.html?homescreen=1',
-       '/?homescreen=1',
-       '/styles/main.css',
-       '/scripts/main.min.js',
-       '/sounds/airhorn.mp3'
+        '/',
+        '/css/style.css',
+        '/js/script.js',
+        '/index.html'      
      ]);
    })
  );
